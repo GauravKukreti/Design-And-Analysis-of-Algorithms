4a0ff0a1e63949f0c454324d902177afa6811fc6
Gaurav Kukreti <73776421+GauravKukreti@users.noreply.github.com>
2022-03-30 09:44:54 +0530

    Create Q1_Insertion_Sort.cpp

---
+++ Lab Assignments/Week 3/Q1_Insertion_Sort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[1000];
+        for(int i=0;i<n;i++)
+          cin>>arr[i];
+        
+        int comp=0,shift=0;
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            comp++;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                comp++;
+                shift++;
+            }
+            arr[j + 1] = key;
+        }
+        
+        printArray(arr, n);
+        cout<<"Comparisions = "<<comp<<endl<<"Shifts = "<<shift<<endl;
+    }
+    return 0;
+}
+
+
+/*
+
+3
+8
+-23 65 -31 76 46 89 45 32
+10
+54 65 34 76 78 97 46 32 51 21
+15
+63 42 223 645 652 31 324 22 553 -12 54 65 86 46 325
+
+*/
